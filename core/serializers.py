35a544d6425990f8c9bd923c301cfc001941ce44
erdem <github@erdemozkol.com>
2019-02-22 15:16:10 +0000

    Implemented domestic export serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from core import fields
+from core.constants import COMPANY_SECTOR_CHOISES, EMPLOYEES_NUMBER_CHOISES, HEARD_ABOUT_CHOISES
 
 
 class BasePageSerializer(serializers.Serializer):
@@ -42,3 +43,17 @@ class WagtailPageSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField()
     slug = serializers.CharField()
+
+
+class DomesticContractForm(serializers.Serializer):
+    contact_name = serializers.CharField()
+    contact_email = serializers.EmailField(label='Contact Email')
+    contact_number = serializers.IntegerField()
+    company_name = serializers.CharField()
+    company_location = serializers.CharField()
+    sector = serializers.ChoiceField(choices=COMPANY_SECTOR_CHOISES)
+    company_website = serializers.URLField()
+    employees_number = serializers.ChoiceField(choices=EMPLOYEES_NUMBER_CHOISES)
+    currently_export = serializers.BooleanField()
+    advertising_feedback = serializers.ChoiceField(choices=HEARD_ABOUT_CHOISES)
+
