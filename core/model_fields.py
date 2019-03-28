25de7c98ae7abc76797e9b6f6057b598a2c26809
Karl Hobley <karl@torchbox.com>
2019-03-28 12:49:11 +0000

    Add segmentisation for markdown fields

--- core/model_fields.py
+++ core/model_fields.py
@@ -1,4 +1,10 @@
+import markdown
+from bs4 import BeautifulSoup
 from django.db.models import TextField
+from markdownify import markdownify as md
+from wagtail_i18n.segments import SegmentValue, TemplateValue
+from wagtail_i18n.segments.html import extract_html_segment, render_html_segment
+from wagtail_i18n.segments.ingest import organise_template_segments
 
 from core import validators as core_validators, widgets
 
@@ -16,3 +22,20 @@ class MarkdownField(TextField):
         }
         defaults.update(kwargs)
         return super(MarkdownField, self).formfield(**defaults)
+
+    def get_translatable_segments(self, value):
+        html = markdown.markdown(value)
+        template, texts = extract_html_segment(html)
+
+        return [
+            TemplateValue('', 'html-formatted-markdown', template, len(texts))
+        ] + [
+            SegmentValue(str(position), text)
+            for position, text in enumerate(texts)
+        ]
+
+    def restore_translated_segments(self, value, segments):
+        format, template, texts = organise_template_segments(segments)
+        assert format == 'html-formatted-markdown'
+        html = render_html_segment(template, texts)
+        return md(html)
